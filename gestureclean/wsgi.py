c5baafb96067f9bdd244e0ea7dd90db44dfcdd2b
Glebys Gonzalez <glebysg@gmail.com>
2017-06-22 15:06:17 -0400

    Setup

---
+++ gestureclean/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for gestureclean project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+sys.path.append("/home/glebys/GC_server")
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestureclean.settings")
+
+application = get_wsgi_application()
