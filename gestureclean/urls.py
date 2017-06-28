66771e6fce32d7e78c5ff47b7099c27d43129ec3
Glebys Gonzalez <glebysg@gmail.com>
2017-06-28 19:42:38 -0400

    add login session

--- gestureclean/urls.py
+++ gestureclean/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.contrib.auth import views as auth_views
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
@@ -21,4 +22,7 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/vacs/')),
     url(r'^vacs/', include('vacs.urls')),
     url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(
+        template_name='vacs/login.html'), name="login"),
 ]
