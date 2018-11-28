8cdb01a6543c8f4f3b1cfeb0651c0522f9dd3130
Glebys Gonzalez <glebysg@gmail.com>
2018-11-28 16:14:35 -0500

    correct database

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GC_server
 Server for the gestureclean project
+Download db_fixed.sqlite3 from the AHRQ dropbox folder:
+AHRQ/Study ||| analysis/Databases
---
+++ gestureclean/within_user_agreement.py
@@ -0,0 +1,10 @@
+# import sys
+# from vacs.models import Command, Experiment, Vac, Evaluation,\
+        # Assignment, Participant, Score, ValAssignment, Validation
+# from vacs.utils import Order
+# from django.contrib.auth import get_user_model
+# import csv
+# import numpy as np
+
+def run():
+    print wii
