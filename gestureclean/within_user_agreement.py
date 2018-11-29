a2fdd0bf802615cdf1d3e11d32517c16de3ea49e
Glebys Gonzalez <glebysg@gmail.com>
2018-11-29 12:30:27 -0500

    created eval matrix

--- gestureclean/within_user_agreement.py
+++ gestureclean/within_user_agreement.py
@@ -27,13 +27,30 @@ for participant in participants:
                     )
             # get groups of evaluations
             for evaluation in evaluations:
-                evaluation = evaluation.split(".")
+                evaluation = evaluation.evaluation.split(".")
                 # create evaluation matrix 9x9x2
-                # The first one is for the less thans and
-                # The second one os for the equals.
-                eval_matrix = np.zeros((9,9,2))
+                # The first one is for the less thans in the upper
+                # triangular and greater thans in the lower trian
+                # gular and  The second one is for the equals.
+                eval_matrix = np.zeros((2,9,9))
+                # [u'9', u'<', u'5', u'<', u'8']
+                for index in range(2):
+                    if evaluation[index + 1] == '<':
+                        m_index = 0
+                    else:
+                        m_index = 1
+                    # Save in the upper triangular if its in order
+                    g1 = int(evaluation[index])
+                    g2 = int(evaluation[index+2])
+                    if g1 < g2:
+                        eval_matrix[m_index,g1-1,g2-1] +=1
+                    # Save in the lower triangular if its in reverse
+                    # order because we are dealing with a "greater than"
+                    else:
+                        eval_matrix[m_index,g2-1,g1-1] +=1
                 # Get the values for each pair (this should be
                 # a dict or a array of two).
+                
 
                 # Get the entropy for each pair
 
@@ -44,7 +61,6 @@ for participant in participants:
             # get the mean of the  means
 
 
-                print evaluation.evaluation
 # make table of  less, greater, equal, and calculate entropy
 # average entropies of each user
 # print a table for each user
